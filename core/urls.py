9d234ecb69e427418b8c69b13a2e2c1e88ac94a3
Nadezhda Khovrich <hope28281@gmail.com>
2022-11-20 23:01:31 +0600

    #40 Детальный просмотр отклика. Добавление сообщений.

--- core/urls.py
+++ core/urls.py
@@ -4,13 +4,15 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 from accounts.views import LoginView, logout_view, RegisterView, AccountDetailView, AccountUpdateView
-from main.views import VacancyListView, VacancyCreateView, VacancyDetailView, VacancyUpdateView, VacancyDateUpdateView
+from main.views import VacancyListView, VacancyCreateView, VacancyDetailView, VacancyUpdateView, VacancyDateUpdateView, ResponseDetailView, MessageCreateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', VacancyListView.as_view(), name='index'),
+
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', logout_view, name='logout'),
+    
     path('register/', RegisterView.as_view(), name='register'),
     path('<str:slug>', AccountDetailView.as_view(), name='account_detail'),
 
@@ -19,6 +21,11 @@ urlpatterns = [
     path('vacancy/create/', VacancyCreateView.as_view(), name='vacancy_create'),
     path('vacancy/<int:pk>', VacancyDetailView.as_view(), name='vacancy_detail'),
     path('vacancy/<int:pk>/update', VacancyUpdateView.as_view(), name='vacancy_update'),
-    path('vacancy/<int:pk>/update_date', VacancyDateUpdateView, name='vacancy_date_update')
+    path('vacancy/<int:pk>/update_date', VacancyDateUpdateView, name='vacancy_date_update'),
+    
+    path('response/<int:pk>', ResponseDetailView.as_view(), name='response_detail'),
+    path('response/<int:pk>/message/create/', MessageCreateView.as_view(), name='message_create')
+
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
--- main/views.py
+++ main/views.py
@@ -4,8 +4,11 @@ from django.urls import reverse
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseForbidden
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-from main.models import Vacancy
+
+from main.models import Vacancy, Message, Response
+from main.forms import MessageCreateForm
 # from accounts.models import Account
 
 
@@ -83,3 +86,65 @@ def VacancyDateUpdateView(request, pk):
     vacancy.save()
     return redirect('account_detail', vacancy.author.username)
     
+
+class ResponseDetailView(LoginRequiredMixin, DetailView):
+    template_name = 'main/detail_response.html'
+    model = Response
+    context_object_name = 'response'
+
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        form = MessageCreateForm()
+        context['form'] = form
+        return context
+
+    def dispatch(self, request, *args, **kwargs):
+        '''
+        pk отклика
+        конкретный отклик по pk
+        автор отклика(откликнувшийся)
+        автор вакансии
+        условие если юзер == откликнувшемуся или юзер == автору вакансии,
+        то разрешаем просмотр отклика
+        '''
+        response_pk = self.kwargs.get('pk')
+        response: Response = get_object_or_404(Response, pk=response_pk)
+        applicant_response = response.applicant
+        author_vacancy = response.vacancy.author
+        if request.user == applicant_response or request.user == author_vacancy:
+            return super().dispatch(request, *args, **kwargs)
+        return self.handle_no_permission()
+
+
+class MessageCreateView(LoginRequiredMixin, CreateView):
+    model = Message
+    form_class = MessageCreateForm
+
+    def form_valid(self, form):
+        self.object: Message = form.save(commit=False)
+        response_pk = self.kwargs.get('pk')
+        self.object.response = get_object_or_404(Response, pk=response_pk)
+        self.object.author = self.request.user
+        self.object.save()
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('response_detail', kwargs={'pk': self.kwargs.get('pk')})
+
+    def dispatch(self, request, *args, **kwargs):
+        '''
+        pk отклика
+        конкретный отклик по pk
+        автор отклика(откликнувшийся)
+        автор вакансии
+        условие если юзер == откликнувшемуся или юзер == автору вакансии,
+        то разрешаем добавить сообщение
+        '''
+        response_pk = self.kwargs.get('pk')
+        response: Response = get_object_or_404(Response, pk=response_pk)
+        applicant_response = response.applicant
+        author_vacancy = response.vacancy.author
+        if request.user == applicant_response or request.user == author_vacancy:
+            return super().dispatch(request, *args, **kwargs)
+        return self.handle_no_permission()
